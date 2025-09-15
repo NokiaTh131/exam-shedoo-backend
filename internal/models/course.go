1b70bd5f035a99e23ece13e1520fd96eebb68004
NokiaTh131 <nokiath131@gmail.com>
2025-09-16 00:30:15 +0700

    feat: add course model

---
+++ internal/models/course.go
@@ -0,0 +1,14 @@
+package models
+
+type Course struct {
+	ID         int      `gorm:"primaryKey;autoIncrement"`
+	CourseCode string   `gorm:"column:course_code;size:6;not null"`
+	Title      string   `gorm:"column:title;not null"`
+	LabSection *string  `gorm:"column:lab_section"`
+	LecSection *string  `gorm:"column:lec_section"`
+	Room       *string  `gorm:"column:room"`
+	Credit     *float32 `gorm:"column:credit"`
+	Days       *string  `gorm:"column:days"`
+	StartTime  *string  `gorm:"column:start_time"`
+	EndTime    *string  `gorm:"column:end_time"`
+}
