d0b01a6cd31d7f2f82852a57fa8e6c464295b69d
NokiaTh131 <nokiath131@gmail.com>
2025-09-24 18:39:48 +0700

    fix: remove user model

--- internal/config/orm.go
+++ internal/config/orm.go
@@ -33,7 +33,7 @@ func New() *Service {
 	if err != nil {
 		panic("failed to connect to database")
 	}
-	db.AutoMigrate(&models.CourseExam{}, &models.Course{}, &models.Enrollment{}, &models.ScrapeCourseJob{}, &models.ScrapeExamJob{}, &models.User{})
+	db.AutoMigrate(&models.CourseExam{}, &models.Course{}, &models.Enrollment{}, &models.ScrapeCourseJob{}, &models.ScrapeExamJob{})
 	dbInstance = &Service{DB: db}
 	return dbInstance
 }
