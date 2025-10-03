7a4786710d492b9901346f4c7b97a54c4e57afaa
NokiaTh131 <nokiath131@gmail.com>
2025-10-03 13:47:57 +0700

    fix: incorrect query for course exam report

--- internal/repositories/course_exam_repository.go
+++ internal/repositories/course_exam_repository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"fmt"
 	"os/exec"
+
 	"shedoo-backend/internal/dto"
 	"shedoo-backend/internal/models"
 
@@ -82,57 +83,57 @@ func (r *CourseExamRepository) GetExamsByStudent(studentCode string) ([]dto.Stud
 func (r *CourseExamRepository) GetExamReport(courseId int) (dto.ProfessorReportCourseResponse, error) {
 	var rows []dto.ExamReport
 	err := r.db.Raw(`
-		SELECT 
-		c.id AS course_id,
-		c.course_code,
-		c.title AS course_name,
-		COUNT(DISTINCT e.student_code) AS student_count,
-		ce.midterm_exam_date AS midterm_date,
-		ce.midterm_exam_start_time AS midterm_start_time,
-		ce.midterm_exam_end_time AS midterm_end_time,
-		ce.final_exam_date AS final_date,
-		ce.final_exam_start_time AS final_start_time,
-		ce.final_exam_end_time AS final_end_time,
-		c.lec_section,
-		c.lab_section
+		SELECT
+				c.id                                  AS course_id,
+				c.course_code,
+				c.title                               AS course_name,
+				COUNT(DISTINCT e.student_code)        AS student_count,
+				ce.midterm_exam_date                  AS midterm_date,
+				ce.midterm_exam_start_time            AS midterm_start_time,
+				ce.midterm_exam_end_time              AS midterm_end_time,
+				ce.final_exam_date                    AS final_date,
+				ce.final_exam_start_time              AS final_start_time,
+				ce.final_exam_end_time                AS final_end_time,
+				c.lec_section,
+				c.lab_section
 		FROM enrollments e
 		JOIN courses c ON e.course_id = c.id
 		LEFT JOIN LATERAL (
-		SELECT 
-			ce.midterm_exam_date, 
-			ce.midterm_exam_start_time,
-			ce.midterm_exam_end_time,
-			ce.final_exam_date,
-			ce.final_exam_start_time,
-			ce.final_exam_end_time
-		FROM course_exams ce
-		WHERE ce.course_code = c.course_code
-		AND (
-			(ce.lec_section = c.lec_section AND ce.lab_section = c.lab_section)
-			OR (ce.lec_section = '000' AND ce.lab_section = '000')
-		)
-		ORDER BY 
-			(ce.lec_section = c.lec_section AND ce.lab_section = c.lab_section) DESC
-		LIMIT 1
+			SELECT
+				ce.midterm_exam_date,
+				ce.midterm_exam_start_time,
+				ce.midterm_exam_end_time,
+				ce.final_exam_date,
+				ce.final_exam_start_time,
+				ce.final_exam_end_time
+			FROM course_exams ce
+			WHERE ce.course_code = c.course_code
+			  AND (
+				  (ce.lec_section = c.lec_section AND ce.lab_section = c.lab_section)
+				  OR
+				  (ce.lec_section = '000' AND ce.lab_section = '000')
+			  )
+			ORDER BY
+				(ce.lec_section = c.lec_section AND ce.lab_section = c.lab_section) DESC
+			LIMIT 1
 		) ce ON TRUE
 		WHERE e.student_code IN (
-		SELECT student_code 
-		FROM enrollments 
-		WHERE course_id = ?
+			SELECT student_code
+			FROM enrollments
+			WHERE course_id = ?
 		)
-		GROUP BY 
-		c.id, c.course_code, c.title, 
-		ce.midterm_exam_date, ce.midterm_exam_start_time, ce.midterm_exam_end_time,
-		ce.final_exam_date, ce.final_exam_start_time, ce.final_exam_end_time,
-		c.lec_section, c.lab_section
-		ORDER BY student_count DESC;
-`, courseId).Scan(&rows).Error
+		GROUP BY
+			c.id, c.course_code, c.title,
+			ce.midterm_exam_date, ce.midterm_exam_start_time, ce.midterm_exam_end_time,
+			ce.final_exam_date, ce.final_exam_start_time, ce.final_exam_end_time,
+			c.lec_section, c.lab_section
+		ORDER BY student_count DESC
+	`, courseId).Scan(&rows).Error
 	if err != nil || len(rows) == 0 {
 		return dto.ProfessorReportCourseResponse{}, err
 	}
 	midtermGroups := map[string]*dto.TermResponse{}
 	finalGroups := map[string]*dto.TermResponse{}
-
 	for _, row := range rows {
 		course := dto.CourseReponse{
 			CourseID:      row.CourseID,
@@ -142,7 +143,6 @@ func (r *CourseExamRepository) GetExamReport(courseId int) (dto.ProfessorReportC
 			LabSection:    derefString(row.LabSection),
 			NumOfStudents: row.StudentCount,
 		}
-
 		if derefString(row.MidtermDate) != "" {
 			key := derefString(row.MidtermDate) + derefString(row.MidtermStartTime) + derefString(row.MidtermEndTime)
 			if _, exists := midtermGroups[key]; !exists {
@@ -155,7 +155,6 @@ func (r *CourseExamRepository) GetExamReport(courseId int) (dto.ProfessorReportC
 			}
 			midtermGroups[key].Courses = append(midtermGroups[key].Courses, course)
 		}
-
 		if derefString(row.FinalDate) != "" {
 			key := derefString(row.FinalDate) + derefString(row.FinalStartTime) + derefString(row.FinalEndTime)
 			if _, exists := finalGroups[key]; !exists {
@@ -169,31 +168,28 @@ func (r *CourseExamRepository) GetExamReport(courseId int) (dto.ProfessorReportC
 			finalGroups[key].Courses = append(finalGroups[key].Courses, course)
 		}
 	}
-
 	midtermResp := []dto.TermResponse{}
 	for _, v := range midtermGroups {
 		midtermResp = append(midtermResp, *v)
 	}
-
 	finalResp := []dto.TermResponse{}
 	for _, v := range finalGroups {
 		finalResp = append(finalResp, *v)
 	}
-
 	response := &dto.ProfessorReportCourseResponse{
 		MidtermResponse: &midtermResp,
 		FinalResponse:   &finalResp,
 	}
-
 	return *response, nil
 }
 
 func (r *CourseExamRepository) ParseAndInsert(pdfPath string, examType string) error {
-    cmd := exec.Command("python3", "internal/script/examdate_from_pdf.py", "--pdf", pdfPath, "--exam_type", examType)
-    output, err := cmd.CombinedOutput()
-    if err != nil {
-        return fmt.Errorf("failed to run Python script: %v, output: %s", err, string(output))
-    }
-    fmt.Println(string(output))
-    return nil
+	cmd := exec.Command("python3", "internal/script/examdate_from_pdf.py", "--pdf", pdfPath, "--exam_type", examType)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to run Python script: %v, output: %s", err, string(output))
+	}
+	fmt.Println(string(output))
+	return nil
 }
+
